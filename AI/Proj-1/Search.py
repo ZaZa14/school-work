d07b21c3ccd0760a410bb78fcaae703bb39ddf8d
inson <djzafar92@mail.ru>
2016-09-17 23:02:57 -0400

    dikjstra is working for one test case, type conversion needs to be fixed

--- AI/Proj-1/Search.py
+++ AI/Proj-1/Search.py
@@ -1,21 +1,109 @@
 #!/usr/bin/python3
+# =================================================
+# Project 1--Graph Search
+# Student: Zafar Mamarakhimov
+# Email: mzafar2@umbc.edu
+# =================================================
 import os
 import sys
 
+from collections import defaultdict, deque
+
+
+# --------------------------------------------------
+# Graph class where nodes connected with each other.
+class Graph(object):
+  # constructor for Graph
+  def __init__(self):
+    self.nodes = set()
+    self.verteces = defaultdict(list)
+    self.weight = {}
+
+  # adding a vertex into the graph
+  def addNode(self, value):
+        self.nodes.add(value)
+        print("added node " + value)
+
+  # connecting two nodes with weight
+  def makeEdge(self, from_node, to_node, distance):
+    self.verteces[from_node].append(to_node)
+    # self.verteces[to_node].append(from_node)
+    self.weight[(from_node, to_node)] = distance
+    print("distance between {} and {} is {}".format(from_node, to_node, distance)) # for debugging purposes
+
+  # returning set of nodes;
+  # nodes are un-ordered
+  def getNodes(self):
+    return self.nodes
+    
+
+
+# calculates shortest path to every node in the graph
+def dikjstra(nodes, start):
+  visited = {start: 0}
+  path = {}
+
+  allNodes = set(graph.getNodes())
+
+  while allNodes:
+    min_node = None
+    for node in allNodes:
+      if node in visited:
+        if min_node is None:
+          min_node = node
+        elif visited[node] < visited[min_node]:
+          min_node = node
+
+    if min_node is None:
+      break
+    allNodes.remove(min_node)
+    current_weight = visited[min_node]
+
+    for edge in nodes.verteces[min_node]:
+      try:
+        weight = int(current_weight) + int(nodes.weight[(min_node, edge)])
+      except:
+        continue
+      if edge not in visited or weight < visited[edge]:
+        visited[edge] = weight
+        path[edge] = min_node
+
+  return visited, path
+
+
+
 
 # Breadth first search
-def bfsSearch(start, goal):
+def bfsSearch(graph, start, goal):
   print("Inside of BFS: start-->" + start + " goal-->" + goal)
 
 
 # Depth first search
-def dfsSearch(start, goal):
+def dfsSearch(graph, start, goal):
   print("Inside of DFS: start-->" + start + " goal-->" + goal)
 
 
-# Uniform Cost Search
-def ucsSearch(start, goal):
+# Uniform Cost Search a.k.a Dikjstra Algorithm
+def ucsSearch(graph,start, goal):
   print("Inside of UCS: start-->" + start + " goal-->" + goal)
+  visited_nodes, path = dikjstra(graph, start)
+
+  # create a queue
+  shortest_path = deque()
+  adjacent_node = path[goal] # go backwards, start from the goal and go to the start
+
+  while adjacent_node != start:
+    shortest_path.appendleft(adjacent_node)
+    adjacent_node = path[adjacent_node]
+
+  shortest_path.appendleft(start)
+  shortest_path.append(goal)
+
+  return list(shortest_path)
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -26,31 +114,39 @@ if __name__ == '__main__':
     inputFile = sys.argv[1]
     start = sys.argv[2]
     goal = sys.argv[3]
-    search = sys.argv[4]    
+    search = sys.argv[4]
 
-  with open(inputFile, 'r') as fp:
-    for line in fp:
-        print(line, end="")
+    graph = Graph()   
 
-  # making search based on the command
-  if(search == "BFS"):
-    bfsSearch(start, goal)
-  elif(search == "DFS"):
-    dfsSearch(start, goal)
+    try:
+      with open(inputFile, 'r') as fp:
+        for line in fp:
+          line = line.replace(" ", "") # removing all white spaces from the string           
+          graph.addNode(line[0])
+          graph.addNode(line[1])
+          graph.makeEdge(line[0], line[1], line[2])
+    except IOError:
+      print("Cannot find the input file")
+
+    print(graph.getNodes())
+
+    # making search based on the command
+    if(search == "BFS"):
+      bfsSearch(graph, start, goal)
+    elif(search == "DFS"):
+      dfsSearch(graph, start, goal)
+    else:
+      print(ucsSearch(graph, start, goal))
+
   else:
-    ucsSearch(start, goal)
+    print("Enter correct number of arguments") # if input is not correct
 
-  hashMap = {1 : [2, 3 ,4], 
-              2 : [5, 6],
-              3 : [7, 8],
-              4 : []}
-  print(hashMap[1])
-  print(hashMap[4])
-  hashMap[4] = [1, 5, 9]
-  print(hashMap[4])
-  print(hashMap.keys())
-
-
-
-
-
+  # hashMap = {1 : [2, 3 ,4], 
+  #             2 : [5, 6],
+  #             3 : [7, 8],
+  #             4 : []}
+  # print(hashMap[1])
+  # print(hashMap[4])
+  # hashMap[4] = [1, 5, 9]
+  # print(hashMap[4])
+  # print(hashMap.keys())
