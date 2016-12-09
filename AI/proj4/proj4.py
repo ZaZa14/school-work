492aa0df636dcde70dad74c8d53e378b68b64e14
inson <djzafar92@mail.ru>
2016-12-09 01:35:48 -0500

    reading txt from a file, removed all non-ascii characters

--- AI/proj4/proj4.py
+++ AI/proj4/proj4.py
@@ -1,4 +1,29 @@
 import word2vec
 import sklearn
+import os
+import glob
+path = '/home/inson/Desktop/AI/school-work/AI/proj4'
+mn = 20
+folders = ([name for name in os.listdir(path)
+           if os.path.isdir(os.path.join(path, name)) and name.startswith("c")]) # get all directories 
+for folder in folders:
+    contents = os.listdir(os.path.join(path,folder)) # get list of contents
+    for file in contents:
+      file_name = 'conservative/' + file
+      try:
+        with open(file_name, 'r') as fp:
+          print("------------------------", file_name, "-----------------------")
+          a = fp.read()                
+          print(a)
+      except IOError:
+        print("Cannot open file ", file )
+    # print(contents[0]) # printing the title of one txt file
+    # try: 
 
+
+    # if len(contents) > mn: # if greater than the limit, print folder and number of contents
+    #     print(folder,len(contents))
+
+
+#print (glob.glob(path))
 print("End of project")
